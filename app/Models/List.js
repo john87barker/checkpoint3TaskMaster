71ecde4be26b56e55c6718e5cd3985da642d9455
john87barker <john87barker@gmail.com>
2021-07-17 08:18:02 -0600

    List template works EXACTLY as it should

--- app/Models/List.js
+++ app/Models/List.js
@@ -13,38 +13,40 @@ export default class List {
         return /*html*/`
        
         <div class="col-md-3 card box-shadow bg-primary ">
-                <div class="row d-flex ">
-            <div class="d-flex col-md-12 text-center" id="lists">
-                <h4>${this.title}</h4>
-            </div>
-
-
-            <div class="col-md-12 bg-light pt-3" id="tasks">
-                <div class="btn-group " data-toggle="buttons">
-                    <label class="btn btn-alert active">
-                        <input type="checkbox" name="complete" id="first">
-                    </label>
-                    <div class="d-flex text-right">
-                        <p class=""> ${this.MyTasks}
-                            <i class="fa fa-trash color-warning" aria-hidden="true"></i>
-                        </p>
-                    </div>
+            <div class="row d-flex ">
+                <div class="d-flex col-md-12 text-center" id="lists">
+                    <h4>${this.title}</h4>
                 </div>
 
-                <div class="row ">
-                    <div class="form-group col-md-10 pl-2 pr-0">
-                        <label for="add task"></label>
 
-                        <input type="text" class="form-control" name="addtask" id=""
-                            placeholder="add task">
+                <div class="col-md-12 bg-light pt-3" id="tasks">
+                    <div class="btn-group " data-toggle="buttons">
+                        <label class="btn btn-alert active">
+                            <input type="checkbox" name="complete" id="first">
+                        </label>
+                        <div class="d-flex text-right">
+                            <p class="">
+                            ${this.MyTasks}
+                                <i class="fa fa-trash color-warning" aria-hidden="true"></i>
+                            </p>
+                        </div>
                     </div>
-                    <div class="d-flex  col-md-2 align-items-center justify-content-center">
-                        <i class="fa fa-plus-square " aria-hidden="true"></i>
-                    </div>
+
+                    <div class="row ">
+                        <div class="form-group col-md-10 pl-2 pr-0">
+                            <label for="add task"></label>
+
+                            <input type="text" class="form-control" name="addtask" id=""
+                                placeholder="add task">
+                        </div>
+                        <div class="d-flex  col-md-2 align-items-center justify-content-center">
+                            <i class="fa fa-plus-square " aria-hidden="true"></i>
+                        </div>
+                    </div>    
                 </div>
                 <hr class="mt-0">
-            </div>`
-
+            </div>
+        </div >`
     }
 
 

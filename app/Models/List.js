667e0a21f4868a4fbcc50dec5066477fa73b33b2
john87barker <john87barker@gmail.com>
2021-07-19 08:35:23 -0600

    messing stuff up

--- app/Models/List.js
+++ app/Models/List.js
@@ -25,11 +25,9 @@ export default class List {
             <div class="col-md-12 bg-light pt-3" id="tasks">
                 <div class="btn-group " data-toggle="buttons">
                     <label class="btn btn-alert active">
-                    
-                        <input type="checkbox" name="complete" id="first">
+                    <input type="checkbox" name="complete" id="first">
                     </label>
                     <div class="d-flex text-dark">
-
                         <ul>
                         
                         ${this.MyTask}
